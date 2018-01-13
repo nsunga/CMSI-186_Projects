8afe078c7bbef62cb9905d21557d8b0a4ec223c9
Nicholas Sunga <nick.sunga95@gmail.com>
2018-01-13 15:29:10 -0800

    pushes ignore class files and added docs to Hello.java

--- homework00/Hello.java
+++ homework00/Hello.java
@@ -1,6 +1,18 @@
+/**
+ * Purpose: Serves to be initial experience with Git, GitHub and how to
+ * create working code for the JVM. The code is a basic "meme" implementation
+ * of HelloWorld
+ *
+ * Input: Nothing
+ *
+ * Output: A basic "hello world"-like greeting
+ *
+ * @author NICHOLAS SUNGA
+ */
+
 public class Hello {
 
   public static void main(String[] args) {
-    System.out.println("hello world");
+    System.out.println("hello world!!!");
 	}
 }
