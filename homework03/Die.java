dd79154417d562765d8a098dbb707d3f3678ea81
Nicholas Sunga <nick.sunga95@gmail.com>
2018-02-08 21:12:14 -0800

    initial attempt at die.java

--- homework03/Die.java
+++ homework03/Die.java
@@ -36,71 +36,81 @@
 public class Die {
 
   /**
-   * private instance data
-   */
-   private int sides;
-   private int pips;
-   private final int MINIMUM_SIDES = 4;
+  * private instance data
+  */
+  private int sides;
+  private int pips;
+  private final int MINIMUM_SIDES = 4;
 
-   // public constructor:
-  /**
-   * constructor
-   * @param nSides int value containing the number of sides to build on THIS Die
-   * @throws       IllegalArgumentException
-   * Note: parameter must be checked for validity; invalid value must throw "IllegalArgumentException"
-   */
-   public Die( int nSides ) {
-     this.sides = nSides;
-   }
 
+  // public constructor:
   /**
-   * Roll THIS die and return the result
-   * @return  integer value of the result of the roll, randomly selected
-   */
-   public int roll() {
-      return 0;
-   }
+  * constructor
+  * @param nSides int value containing the number of sides to build on THIS Die
+  * @throws       IllegalArgumentException
+  * Note: parameter must be checked for validity; invalid value must throw "IllegalArgumentException"
+  */
+  public Die( int nSides ) {
+    try { if (nSides < MINIMUM_SIDES) { throw new IllegalArgumentException(); } else { this.sides = nSides; } }
+    catch(IllegalArgumentException e) { System.exit("int sides too small"); }
+
+    // in real life, some side needs to be facing up
+    this.pips = Math.random() * this.sides + 1;
+  }
 
   /**
-   * Get the value of THIS die to return to the caller; note that the way
-   *  the count is determined is left as a design decision to the programmer
-   *  For example, what about a four-sided die - which face is considered its
-   *  "value"?
-   * @return the pip count of THIS die instance
-   */
-   public int getValue() {
-      return 0;
-   }
+  * Roll THIS die and return the result
+  * @return  integer value of the result of the roll, randomly selected
+  */
+  public int roll() {
+    this.pips = (int)Math.random() * this.sides + 1;
+    return this.pips;
+  }
 
   /**
-   * @param  int  the number of sides to set/reset for this Die instance
-   * @return      The new number of sides, in case anyone is looking
-   * @throws      IllegalArgumentException
-   */
-   public void setSides( int sides ) {
-   }
+  * Get the value of THIS die to return to the caller; note that the way
+  *  the count is determined is left as a design decision to the programmer
+  *  For example, what about a four-sided die - which face is considered its
+  *  "value"?
+  * @return the pip count of THIS die instance
+  */
+  public int getValue() {
+    return this.pips;
+  }
 
   /**
-   * Public Instance method that returns a String representation of THIS die instance
-   * @return String representation of this Die
-   */
-   public String toString() {
-      return "";
-   }
+  * @param  int  the number of sides to set/reset for this Die instance
+  * @return      The new number of sides, in case anyone is looking
+  * @throws      IllegalArgumentException
+  */
+  public void setSides( int sides ) {
+    try { if (sides < MINIMUM_SIDES) { throw new IllegalArgumentException(); } else { this.sides = nSides; } }
+    catch(IllegalArgumentException e) { System.exit("int sides too small"); }
+
+    this.pips = (int)Math.random() * this.sides + 1;
+  }
 
   /**
-   * Class-wide method that returns a String representation of THIS die instance
-   * @return String representation of this Die
-   */
-   public static String toString( Die d ) {
-      return "";
-   }
+  * Public Instance method that returns a String representation of THIS die instance
+  * @return String representation of this Die
+  */
+  public String toString() {
+    return "[Sides: " + this.sides + "]";
+  }
 
   /**
-   * A little test main to check things out
-   */
-   public static void main( String[] args ) {
-      System.out.println( "Hello world from the Die class..." );
-   }
+  * Class-wide method that returns a String representation of THIS die instance
+  * @return String representation of this Die
+  */
+  public static String toString( Die d ) {
+    return "pips: " + d.getValue();
+  }
+
+  /**
+  * A little test main to check things out
+  */
+  public static void main( String[] args ) {
+    System.out.println( "Hello world from the Die class..." );
+  }
 
 }
