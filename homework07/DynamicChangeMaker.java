69a765e7de3b59809ff6d1bb200e2038a755cfe4
Nicholas Sunga <nick.sunga95@gmail.com>
2018-04-21 13:20:33 -0700

    init col zero

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -4,7 +4,18 @@ public final class DynamicChangeMaker {
   private DynamicChangeMaker() { }
 
   public static Tuple makeChangeWithDynamicProgramming(int[] denomination, int target) {
+    Tuple[][] table = new Tuple[denomination.length][target + 1];
+
+    initColZero(table, denomination.length);
+
+    Tuple combinations = new Tuple(denomination.length);
     Arrays.sort(denomination);
     return new Tuple(0);
+  }
+
+  private static void initColZero(Tuple[][] table, int coins) {
+    for (int i = 0; i < coins; i++) {
+      table[i][0] = new Tuple(coins);
+    }
   }
 }
