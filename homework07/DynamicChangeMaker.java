e8fcd4063583c4646c89e1e0b4635a705c4e4a5f
Nicholas Sunga <nick.sunga95@gmail.com>
2018-04-21 11:56:46 -0700

    we good

---
+++ homework07/DynamicChangeMaker.java
@@ -0,0 +1,8 @@
+public final class DynamicChangeMaker {
+  private DynamicChangeMaker () { // private constructor
+  }
+
+  public static Tuple makeChangeWithDynamicProgramming(int[] denomination, int target) {
+    return new Tuple(0);
+  }
+}
