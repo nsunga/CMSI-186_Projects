113bfc656c24c9937a166917f23c0782646a8a1d
Nicholas Sunga <nick.sunga95@gmail.com>
2018-04-21 12:16:07 -0700

    sort dat bish

--- homework07/DynamicChangeMaker.java
+++ homework07/DynamicChangeMaker.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public final class DynamicChangeMaker {
-  private DynamicChangeMaker () { // private constructor
-  }
+  private DynamicChangeMaker() { }
 
   public static Tuple makeChangeWithDynamicProgramming(int[] denomination, int target) {
+    Arrays.sort(denomination);
     return new Tuple(0);
   }
 }
